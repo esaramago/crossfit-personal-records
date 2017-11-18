16c406a62cb5c276e837c8df7ae8d0bb1342ff21
Emanuel Saramago <reg@esaramago.com>
2017-11-18 22:26:02 +0000

    dialog

--- app/components/dialog-window.js
+++ app/components/dialog-window.js
@@ -10,23 +10,25 @@ Vue.component("dialog-window", {
     },
     template: `
         <dialog class="mdl-dialog" v-bind:open="isDialogOpen">
-            <h2 class="mdl-dialog__title">Add record</h2>
-            <div class="mdl-dialog__content">
-                <form>
-                    <div class="mdl-textfield mdl-js-textfield">
-                        <input class="mdl-textfield__input" type="text" id="inputExercise" v-model="inputExercise" v-on:input="validateInputs">
-                        <label class="mdl-textfield__label" for="inputExercise">Exercise</label>
-                    </div>
-                    <div class="mdl-textfield mdl-textfield--sm mdl-textfield--centered mdl-js-textfield">
-                        <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="inputWeight" v-model="inputWeight" v-on:input="validateInputs">
-                        <label class="mdl-textfield__label" for="inputWeight">Weight</label>
-                        <span class="mdl-textfield__error">Input is not a number!</span>
-                    </div>
-                </form>
-            </div>
-            <div class="mdl-dialog__actions">
-                <button type="button" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" disabled>Add</button>
-                <button type="button" class="mdl-button close" v-on:click="closeDialog">Cancel</button>
+            <div class="mdl-dialog__container">
+                <h2 class="mdl-dialog__title">Add record</h2>
+                <div class="mdl-dialog__content">
+                    <form>
+                        <div class="mdl-textfield mdl-js-textfield">
+                            <input class="mdl-textfield__input" type="text" id="inputExercise" v-model="inputExercise" v-on:input="validateInputs">
+                            <label class="mdl-textfield__label" for="inputExercise">Exercise</label>
+                        </div>
+                        <div class="mdl-textfield mdl-textfield--sm mdl-textfield--centered mdl-js-textfield">
+                            <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="inputWeight" v-model="inputWeight" v-on:input="validateInputs">
+                            <label class="mdl-textfield__label" for="inputWeight">Weight</label>
+                            <span class="mdl-textfield__error">Input is not a number!</span>
+                        </div>
+                    </form>
+                </div>
+                <div class="mdl-dialog__actions">
+                    <button type="button" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" disabled>Add</button>
+                    <button type="button" class="mdl-button close" v-on:click="closeDialog">Cancel</button>
+                </div>
             </div>
         </dialog>
     `,
