e9f25bdac7cc1b9ee8b37e5e28161e93591ebead
Emanuel Saramago <reg@esaramago.com>
2017-11-24 08:22:11 +0000

    uglify error

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('lint:test', () => {
 gulp.task('html', ['styles', 'scripts'], () => {
   return gulp.src('app/*.html')
     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
-    //.pipe($.if(/\.js$/, $.uglify({compress: {drop_console: true}})))
+    //.pipe($.if(/\.js$/, $.uglify({compress: {drop_console: true}}))) // this throws an error on deploy
     .pipe($.if(/\.css$/, $.cssnano({safe: true, autoprefixer: false})))
     .pipe($.if(/\.html$/, $.htmlmin({
       collapseWhitespace: true,
